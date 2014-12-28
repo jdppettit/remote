13692c8391fe8c674c96f5bb51b58fb5fb4da125
silverp1 <jpettit@pettitservers.com>
2014-12-27 22:55:40 -0500

    Map now fully functional

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,18 @@
 var myLatlng = new google.maps.LatLng(0,0);
-        var map;
+var officeLoc = new google.maps.LatLng(40.72339, -74.00537)
+var map;
         function initialize() {
                 geocoder = new google.maps.Geocoder();
                 var mapOptions = {
                         zoom: 1,
                         center: myLatlng
                 };
-
+		
                 map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
         }
 
         google.maps.event.addDomListener(window, 'load', initialize);
+
         refreshMap();
 
         function addRecord() {
@@ -34,11 +36,8 @@ var myLatlng = new google.maps.LatLng(0,0);
 
         function refreshMap() {
                 $.get('/get', function(data) {
-                        console.log(data);
-                        console.log(data['records'][0]);
                         for(index = 0; index < data['records'].length; ++index ) {
                                 var marker_loc = new google.maps.LatLng(data['records'][index]['latitude'], data['records'][index]['longitude'])
-                                console.log(data['records'][index]['latitude']);
                                 var marker = new google.maps.Marker({
                                         position: marker_loc,
                                         map: map,
